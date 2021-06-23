00dc0259207134a2b541e46573c4335944896c2c
zhenhao <50682966+rchuzh99@users.noreply.github.com>
2021-06-23 16:51:30 +0800

    login

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ from pathlib import Path
 import streamlit
 ROOT = Path(__file__).parent  # ROOT folder
 sys.path.insert(0, str(Path(ROOT, 'lib')))  # ./lib
-print(sys.path[0])
+# print(sys.path[0])
 #
 #--------------------Logger-------------------------#
 import logging
@@ -28,17 +28,23 @@ import streamlit as st
 
 # DEFINE Web APP page configuration
 try:
-    st.set_page_config(page_title="Label Studio Test",
-                       page_icon="random", layout='wide')
+    st.set_page_config(page_title="Integrated Vision Inspection System",
+                       page_icon="static/media/shrdc_image/shrdc_logo.png", layout='wide')
 except:
     st.beta_set_page_config(page_title="Label Studio Test",
                             page_icon="random", layout='wide')
+
+#------------------IMPORT for PAGES-------------------#
+from pages import login
+
+
+#----------------------------------------------------#
 
 
 # PAGES Dictionary
 # Import as modules from "./lib/pages"
 PAGES = {
-    "LOGIN": "",
+    "LOGIN": login,
     "PROJECT": "",
     "DATASET": "",
     "INFERENCE": "",
@@ -46,7 +52,19 @@ PAGES = {
 
 
 def main():
-    print("Hi")
+    #------------------START------------------------#
+    with st.sidebar.beta_container():
+
+        st.image("../resources/MSF-logo.gif", use_column_width=True)
+    with st.beta_container():
+        st.title("Integrated Vision Inspection System", anchor='title')
+
+        st.header(
+            "(Integrated by Malaysian Smart Factory 4.0 Team at SHRDC)", anchor='heading')
+    st.markdown("""___""")
+#-------------------------------------------#
+
+    PAGES["LOGIN"].write()
 
 
 if __name__ == "__main__":
--- src/lib/pages/login.py
+++ src/lib/pages/login.py
@@ -5,4 +5,62 @@ Author: Chu Zhen Hao
 Organisation: Malaysian Smart Factory 4.0 Team at Selangor Human Resource Development Centre (SHRDC)
 """
 import streamlit as st
+from streamlit import cli as stcli
+from pathlib import Path
+from time import sleep
+import sys
+# with __name__ is not "__main__":
+#     PARENT = Path(__file__).parents[3]  # Parent folder
+#     RESOURCE = Path(PARENT, "resources")
 
+# left, mid, right = st.beta_columns([1,3,1])
+
+
+def is_authenticated(username, password):  # WIP
+    """Some function here to obtain User Credentials from Database
+    WIP"""
+
+    return username == "admin" and password == "shrdc"
+
+
+def write():
+    left, mid_login, right = st.beta_columns([1, 3, 1])
+    with mid_login:
+        with st.form("login", clear_on_submit=True):
+            st.write("## Login")
+            username = st.text_input(
+                "Username", value="Please enter your username", key="username")
+            pswrd = st.text_input("Password", value="",
+                                  key="safe", type="password")
+            submit_login = st.form_submit_button("Log In")
+        # st.write(f"{username},{pswrd}")
+        success_place = st.empty()
+        if submit_login:
+            if is_authenticated(username, pswrd):
+                # st.balloons()
+                # st.header("Welcome In")
+                success_place.success("Welcome in")
+                success_side = st.sidebar.empty()
+
+                success_side.write("# Welcome In 👍")
+                sleep(1)
+                success_place.empty()
+                success_side.empty()
+
+            elif pswrd:
+                st.error(
+                    "User entered wrong username or password. Please enter again.")
+
+
+def main():
+    write()
+
+
+if __name__ == "__main__":
+    if st._is_running_with_streamlit:
+        PARENT = Path(__file__).parents[3]  # Parent folder
+        RESOURCE = Path(PARENT, "resources")
+        main()
+    else:
+        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.exit(stcli.main())
