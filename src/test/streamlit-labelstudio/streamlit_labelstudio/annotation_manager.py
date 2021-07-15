64aff1da1da2a06013ba57588312f336c57cf61e
zhenhao <50682966+rchuzh99@users.noreply.github.com>
2021-07-15 20:26:31 +0800

    Complement src

---
+++ src/test/streamlit-labelstudio/streamlit_labelstudio/annotation_manager.py
@@ -0,0 +1,66 @@
+"""
+Title: Annotation Manager
+Date: 15/7/2021
+Author: Chu Zhen Hao
+Organisation: Malaysian Smart Factory 4.0 Team at Selangor Human Resource Development Centre (SHRDC)
+"""
+
+import sys
+from pathlib import Path
+import psycopg2
+from typing import List, Dict
+from datetime import datetime
+import streamlit as st
+from streamlit import cli as stcli  # Add CLI so can run Python script directly
+from streamlit import session_state as SessionState
+
+# >>>>>>>>>>>>>>>>>>>>>>TEMP>>>>>>>>>>>>>>>>>>>>>>>>
+
+SRC = Path(__file__).resolve().parents[2]  # ROOT folder -> ./src
+LIB_PATH = SRC / "lib"
+TEST_MODULE_PATH = SRC / "test" / "test_page" / "module"
+
+for path in sys.path:
+    if str(LIB_PATH) not in sys.path:
+        sys.path.insert(0, str(LIB_PATH))  # ./lib
+    else:
+        pass
+
+    if str(TEST_MODULE_PATH) not in sys.path:
+        sys.path.insert(0, str(TEST_MODULE_PATH))
+    else:
+        pass
+# >>>> User-defined Modules >>>>
+from path_desc import chdir_root
+from core.utils.log import log_info, log_error  # logger
+from data_manager.database_manager import init_connection
+
+# <<<<<<<<<<<<<<<<<<<<<<TEMP<<<<<<<<<<<<<<<<<<<<<<<
+
+
+class Results:
+    def __init__(self, from_name, to_name, type, value) -> None:
+        self.from_name: str = from_name
+        self.to_name: str = to_name
+        self.type: str = type
+        self.value: List[Dict] = value
+
+
+class Annotations:
+    def __init__(self) -> None:
+        self.id: int = 0
+        self.completed_by: Dict = {}  # user_id, email, first_name, last_name
+        self.was_cancelled: bool = False
+        self.ground_truth: bool = True
+        self.created_at: datetime = datetime.now().astimezone()
+        self.updated_at: datetime = datetime.now().astimezone()
+        self.lead_time: float = 0
+        self.task: int = 0
+        self.results = Results()  # or Dict?
+
+
+def upload_results(results, annotation_type_id, project_id, users_id, task_id):
+    # upload results to annotations
+    #results, annotation_type_id,project_id,users_id,task_id
+    # TODO is it neccessary to have annotation type id?
+    print("Hi")
