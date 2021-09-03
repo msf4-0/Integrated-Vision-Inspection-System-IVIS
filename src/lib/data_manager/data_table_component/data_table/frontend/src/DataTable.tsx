b37eb5520058033d70cfd9f2b75a5fdd69cc43b0
zhenhao <50682966+rchuzh99@users.noreply.github.com>
2021-09-03 10:43:03 +0800

    add apache license header to commited files

--- src/lib/data_manager/data_table_component/data_table/frontend/src/DataTable.tsx
+++ src/lib/data_manager/data_table_component/data_table/frontend/src/DataTable.tsx
@@ -1,3 +1,23 @@
+/* 
+Copyright (C) 2021 Selangor Human Resource Development Centre
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+
+Copyright (C) 2021 Selangor Human Resource Development Centre
+SPDX-License-Identifier: Apache-2.0
+========================================================================================
+*/
+
 import { ReactElement, useEffect, useState } from "react";
 import {
   Streamlit,
@@ -21,10 +41,9 @@ interface ComponentTheme {
   font: string | undefined;
 }
 
-// TODO #134 Pending Text Wrap for Cells 
+// TODO #134 Pending Text Wrap for Cells
 
 function DataTable({ args, theme }: ComponentProps): ReactElement {
-
   //Define Rows and Columns
   const rows: GridRowsProp = args.rows;
   console.log(rows);
@@ -180,7 +199,7 @@ function DataTable({ args, theme }: ComponentProps): ReactElement {
     }
     return frameHeight;
   }
-  
+
   const frameHeight = frameHeightCalc(pageSize) + offset;
   console.log("Rows length", rows.length);
 
--- src/lib/project/project_management.py
+++ src/lib/project/project_management.py
@@ -3,6 +3,24 @@ Title: Project Management
 Date: 21/7/2021
 Author: Chu Zhen Hao
 Organisation: Malaysian Smart Factory 4.0 Team at Selangor Human Resource Development Centre (SHRDC)
+
+Copyright (C) 2021 Selangor Human Resource Development Centre
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+
+Copyright (C) 2021 Selangor Human Resource Development Centre
+SPDX-License-Identifier: Apache-2.0
+========================================================================================
 """
 
 import sys
