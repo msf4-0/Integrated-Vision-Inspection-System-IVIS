b9b18273171375f7f54fdbfd29f42d420af50856
zhenhao <50682966+rchuzh99@users.noreply.github.com>
2021-09-15 16:44:23 +0800

    -Add videoprops to python requirements -Fix 'admin' no hash issue

--- src/lib/user/user_SQL.sql
+++ src/lib/user/user_SQL.sql
@@ -1,20 +1,20 @@
 -- Query User details
 SELECT
-    emp_id,
-    username,
-    first_name,
-    last_name,
-    email,
-    department,
-    position,
-    (
+    emp_id
+    , username
+    , first_name
+    , last_name
+    , email
+    , department
+    , position
+    , (
         SELECT
             r.name
         FROM
             public.roles r
         WHERE
-            r.id = roles_id) AS "Role",
-    (
+            r.id = roles_id) AS "Role"
+    , (
         SELECT
             x.name
         FROM
@@ -33,4 +33,34 @@ SET
     last_activity = datetime.now().astimezone ()
 WHERE
     id = % s;
+
+
+/* Insert User  */
+INSERT INTO public.users (
+    emp_id
+    , first_name
+    , last_name
+    , email
+    , department
+    , position
+    , username
+    , roles_id
+    , psd)
+VALUES (
+    % s
+    , % s
+    , % s
+    , % s
+    , % s
+    , % s
+    , % s
+    , (
+        SELECT
+            r.id
+        FROM
+            public.roles r
+        WHERE
+            r.name = % s) , % s)
+RETURNING
+    id;
 
